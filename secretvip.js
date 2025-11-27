d837e4dac12c5ce13c21bf3d0a56c9ea45c6293a
B!guU <biguueditz@gmail.com>
2025-11-27 16:31:05 +0530

    Add files via upload

---
+++ secretvip.js
@@ -0,0 +1,29 @@
+/******************************
+[rewrite_local]
+# > Wink Vip BiguuDev
+^https?:\/\/api-sub\.meitu\.com\/v2\/user\/vip_info_by_group\.json url script-response-body
+[mitm] 
+hostname = api-sub.meitu.com
+*******************************/
+
+
+
+{
+  "code": 0,
+  "error_code": "00000",
+  "message": "success",
+  "data": {
+    "account_type": 2,
+    "account_id": "1230818232",
+    "is_vip": true,
+    "use_vip": true,
+    "limit_type": 0,
+    "have_valid_contract": true,
+    "show_renew_flag": false,
+    "show_renew_flag_abroad": false,
+    "in_trial_period": false,
+    "in_grace_period": true
+  },
+  "request_id": "ec46c484507048139023381c49036660",
+  "success": true
+}
