72533d684e049b8d5c21ebae37b6899ae829b704
B!guU <biguueditz@gmail.com>
2025-11-27 16:37:57 +0530

    Add files via upload

---
+++ BizScret.js
@@ -0,0 +1,344 @@
+{
+  "code": 0,
+  "error_code": "00000",
+  "message": "success",
+  "data": [
+    {
+      "sub_period_duration": 1,
+      "product_price": {
+        "price": "0",
+        "money_unit": "USD",
+        "money_symbol": "$",
+        "format_price": "$0",
+        "format_price_no_symbol": "0",
+        "price_show_flag": false
+      },
+      "sub_period": 1,
+      "promotions": [],
+      "country_code": "LK",
+      "product_id": "6889144998810643498",
+      "app_id": "6829803307010000000",
+      "platform": 2,
+      "product_type": 2,
+      "product_type_extend": 0,
+      "sub_period_desc": "month",
+      "third_product_id": "com.meitu.wink.autorenew.vip2",
+      "group_id": "6889140818175880200",
+      "product_name": "Monthly",
+      "product_desc": "",
+      "label_new_user": "",
+      "label_old_user": "",
+      "product_status": 1,
+      "promote_product_price": {
+        "price": "999",
+        "money_unit": "USD",
+        "money_symbol": "$",
+        "format_price": "$0",
+        "format_price_no_symbol": "0",
+        "price_show_flag": false
+      },
+      "preferred": 0,
+      "bottom_explain": {
+        "explain2": "10000000000 extra credits per month for VIP. Valid for 31 days and will be reset upon membership expiration.",
+        "icon2": 1
+      },
+      "check_box": {
+        "show_flag": true,
+        "must_check": true,
+        "explain": "Auto-renews at $0/month. You can cancel anytime.",
+        "question_mark_flag": true,
+        "protocol_type": 0,
+        "check_tips": "Please agree to the Membership Agreement first."
+      },
+      "button_explain": {
+        "main_explain": "Continue"
+      },
+      "product_buy_flag": 1,
+      "countdown_flag": 0,
+      "countdown_time": "0",
+      "countdown_end_time": "0",
+      "ios_pay_channel": 1,
+      "commodity_id": "4",
+      "commodity_config": {},
+      "product_style": 0,
+      "first_pay_channel": 0
+    },
+    {
+      "sub_period_duration": 1,
+      "product_price": {
+        "price": "0",
+        "money_unit": "CNY",
+        "money_symbol": "¥",
+        "format_price": "¥0",
+        "format_price_no_symbol": "0",
+        "price_show_flag": false
+      },
+      "sub_period": 1,
+      "promotions": [],
+      "country_code": "LK",
+      "product_id": "7036995469524098850",
+      "app_id": "6829803307010000000",
+      "platform": 2,
+      "product_type": 4,
+      "product_type_extend": 0,
+      "sub_period_desc": "月",
+      "third_product_id": "com.meitu.wink.consume.ImageQualityPE",
+      "product_name": "单次购买",
+      "product_desc": "保存本次画质修复效果",
+      "label_new_user": "",
+      "label_old_user": "",
+      "customize_desc": "保存本次效果",
+      "promotion_banner": "",
+      "mating_desc": "Permanently valid",
+      "product_status": 1,
+      "promote_product_price": {
+        "price": "0",
+        "money_unit": "CNY",
+        "money_symbol": "¥",
+        "format_price": "¥0",
+        "format_price_no_symbol": "0",
+        "price_show_flag": false
+      },
+      "preferred": 0,
+      "bottom_explain": {},
+      "check_box": {
+        "show_flag": false,
+        "must_check": false,
+        "protocol_type": 0,
+        "check_tips": "Please agree to the Membership Agreement first."
+      },
+      "button_explain": {
+        "main_explain": "Buy now for ¥0"
+      },
+      "product_buy_flag": 0,
+      "countdown_flag": 0,
+      "countdown_time": "0",
+      "countdown_end_time": "0",
+      "ios_pay_channel": 1,
+      "commodity_id": "wink.portrait.enhance",
+      "commodity_config": {
+        "count": 1,
+        "unit": 1,
+        "limit_type": 1
+      },
+      "product_style": 0,
+      "first_pay_channel": 2
+    },
+    {
+      "sub_period_duration": 1,
+      "product_price": {
+        "price": "0",
+        "money_unit": "USD",
+        "money_symbol": "$",
+        "format_price": "$0",
+        "format_price_no_symbol": "0",
+        "price_show_flag": false
+      },
+      "sub_period": 3,
+      "promotions": [],
+      "country_code": "LK",
+      "product_id": "6889145337106426924",
+      "app_id": "6829803307010000000",
+      "platform": 2,
+      "product_type": 1,
+      "product_type_extend": 0,
+      "sub_period_desc": "year",
+      "third_product_id": "com.meitu.wink.nonrenew.vip1",
+      "product_name": "1 Year",
+      "product_desc": "",
+      "label_new_user": "",
+      "label_old_user": "",
+      "product_status": 1,
+      "promote_product_price": {
+        "price": "0",
+        "money_unit": "USD",
+        "money_symbol": "$",
+        "format_price": "$0",
+        "format_price_no_symbol": "0",
+        "price_show_flag": false
+      },
+      "preferred": 0,
+      "bottom_explain": {
+        "explain": "Validity period will be automatically extended after purchase.",
+        "icon": 0,
+        "explain2": "10000000000 extra credits per month for VIP. Valid for 31 days and will be reset upon membership expiration.",
+        "icon2": 1
+      },
+      "check_box": {
+        "show_flag": false,
+        "must_check": false,
+        "protocol_type": 0,
+        "check_tips": "Please agree to the Membership Agreement first."
+      },
+      "button_explain": {
+        "main_explain": "Moded By BiguuDev"
+      },
+      "product_buy_flag": 0,
+      "countdown_flag": 0,
+      "countdown_time": "0",
+      "countdown_end_time": "0",
+      "ios_pay_channel": 1,
+      "commodity_id": "4",
+      "commodity_config": {},
+      "product_style": 0,
+      "first_pay_channel": 0
+    },
+    {
+      "sub_period_duration": 1,
+      "product_price": {
+        "price": "4999",
+        "money_unit": "USD",
+        "money_symbol": "$",
+        "format_price": "$0",
+        "format_price_no_symbol": "0",
+        "product_price_explain": "Yearly $0/mo",
+        "price_show_flag": false
+      },
+      "sub_period": 3,
+      "promotions": [],
+      "country_code": "LK",
+      "product_id": "7317726264905978203",
+      "app_id": "6829803307010000000",
+      "platform": 2,
+      "product_type": 2,
+      "product_type_extend": 0,
+      "sub_period_desc": "year",
+      "third_product_id": "com.meitu.wink.autorenew.vip22",
+      "group_id": "6889140818175880200",
+      "product_name": "Yearly (Auto-renewable)",
+      "product_desc": "",
+      "label_new_user": "100% OFF",
+      "label_old_user": "100% OFF",
+      "customize_desc": "",
+      "promote_desc": "",
+      "promotion_banner": "100% OFF",
+      "product_status": 1,
+      "promote_product_price": {
+        "price": "0",
+        "money_unit": "USD",
+        "money_symbol": "$",
+        "format_price": "$0",
+        "format_price_no_symbol": "0",
+        "product_price_explain": "Yearly $0/mo",
+        "price_show_flag": false
+      },
+      "preferred": 0,
+      "bottom_explain": {
+        "explain2": "10000000000 extra credits per month for VIP. Valid for 31 days and will be reset upon membership expiration.",
+        "icon2": 1
+      },
+      "check_box": {
+        "show_flag": true,
+        "must_check": true,
+        "explain": "Auto-renews at $0/year. You can cancel anytime.",
+        "question_mark_flag": true,
+        "protocol_type": 0,
+        "check_tips": "Please agree to the Membership Agreement first."
+      },
+      "button_explain": {
+        "main_explain": "Continue"
+      },
+      "product_buy_flag": 1,
+      "countdown_flag": 0,
+      "countdown_time": "0",
+      "countdown_end_time": "0",
+      "ios_pay_channel": 1,
+      "commodity_id": "4",
+      "commodity_config": {},
+      "product_style": 0,
+      "first_pay_channel": 0
+    },
+    {
+      "sub_period_duration": 1,
+      "product_price": {
+        "price": "4999",
+        "money_unit": "USD",
+        "money_symbol": "$",
+        "format_price": "$0",
+        "format_price_no_symbol": "0",
+        "price_show_flag": false
+      },
+      "sub_period": 3,
+      "promotions": [],
+      "country_code": "LK",
+      "product_id": "6889162865299975354",
+      "app_id": "6829803307010000000",
+      "platform": 2,
+      "product_type": 2,
+      "product_type_extend": 0,
+      "sub_period_desc": "year",
+      "third_product_id": "com.meitu.wink.autorenew.vip1",
+      "group_id": "6889140818175880200",
+      "product_name": "Yearly",
+      "product_desc": "",
+      "label_new_user": "100% OFF",
+      "label_old_user": "100% OFF",
+      "promotion_banner": "100% OFF",
+      "product_status": 1,
+      "promote_product_price": {
+        "price": "0",
+        "money_unit": "USD",
+        "money_symbol": "$",
+        "format_price": "$0",
+        "format_price_no_symbol": "0",
+        "price_show_flag": false
+      },
+      "all_promotions": [
+        {
+          "promotion_id": "6889396674029187362",
+          "promotion_name": "",
+          "third_promotion_code": "",
+          "promotion_type": 2,
+          "promotion_price": {
+            "price": "0",
+            "money_unit": "USD",
+            "money_symbol": "$",
+            "price_show_flag": false
+          },
+          "promotion_duration": {
+            "duration": 7,
+            "period": 0
+          },
+          "activity_tag": "",
+          "outer_show_channel": {
+            "pay_channel": "alipay",
+            "channel_name": "Pay with Alipay"
+          },
+          "give_period": -1,
+          "promotion_free_gifts": [],
+          "promotion_gifts": [],
+          "gift_multi_language": [],
+          "enjoy_type": 0,
+          "enjoy_num": 0
+        }
+      ],
+      "preferred": 1,
+      "bottom_explain": {
+        "explain2": "10000000000 extra credits per month for VIP. Valid for 31 days and will be reset upon membership expiration.",
+        "icon2": 1
+      },
+      "check_box": {
+        "show_flag": true,
+        "must_check": true,
+        "explain": "Auto-renews at $0/year. You can cancel anytime.",
+        "question_mark_flag": true,
+        "protocol_type": 0,
+        "check_tips": "Please agree to the Membership Agreement first."
+      },
+      "button_explain": {
+        "main_explain": "Continue"
+      },
+      "product_buy_flag": 0,
+      "countdown_flag": 0,
+      "countdown_time": "0",
+      "countdown_end_time": "0",
+      "ios_pay_channel": 1,
+      "commodity_id": "4",
+      "commodity_config": {},
+      "product_style": 0,
+      "first_pay_channel": 0
+    }
+  ],
+  "request_id": "bdf59373392842169ebee33350b74fac",
+  "success": true
+}
