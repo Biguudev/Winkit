6ec19e569e97debe391cd93e305b6f302063d0ea
B!guU <biguueditz@gmail.com>
2025-12-11 20:28:44 +0530

    Add files via upload

---
+++ balance.js
@@ -0,0 +1,14 @@
+{
+  "code": 0,
+  "error_code": "00000",
+  "message": "success",
+  "data": {
+    "meiye_balance": "99",
+    "total_amount": "99",
+    "meiye_freeze_amount": "0",
+    "my_credits": "My Credits",
+    "meidou_balance": "99"
+  },
+  "request_id": "",
+  "success": true
+}
