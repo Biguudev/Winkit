8263de0851d4a8e25a0ea942b8fa192656ca6525
B!guU <biguueditz@gmail.com>
2026-01-11 00:04:45 +0530

    Update balance.js

--- balance.js
+++ balance.js
@@ -1,14 +1,22 @@
-{
-  "code": 0,
-  "error_code": "00000",
-  "message": "success",
-  "data": {
-    "meiye_balance": "99",
-    "total_amount": "99",
+/******************************
+[rewrite_local]
+# > Wink Credit
+^https?:\/\/api-sub\.meitu\.com\/v1/\virtual/\account/\balance\.json url script-response-body
+[mitm] 
+hostname = api-sub.meitu.com
+*******************************/
+
+var objc = JSON.parse($response.body);
+
+objc = {
+    "meiye_balance": "25",
+    "total_amount": "25",
     "meiye_freeze_amount": "0",
     "my_credits": "My Credits",
-    "meidou_balance": "99"
+    "meidou_balance": "0"
   },
-  "request_id": "",
+  "request_id": "68bd3c5147be42099e14867a08519e0a",
   "success": true
-}
+};
+$done({
+    body: JSON.stringify(obj)
