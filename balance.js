be8fcff533625c9f4d08bba11c817e64578b5a46
B!guU <biguueditz@gmail.com>
2026-03-07 16:03:46 +0530

    Refactor balance response handling in balance.js

--- balance.js
+++ balance.js
@@ -1,22 +1,11 @@
-/******************************
-[rewrite_local]
-# > Wink Credit
-^https?:\/\/api-sub\.meitu\.com\/v1/\virtual/\account/\balance\.json url script-response-body
-[mitm] 
-hostname = api-sub.meitu.com
-*******************************/
+var body = $response.body;
+var obj = JSON.parse(body);
 
-var objc = JSON.parse($response.body);
+if (obj.data) {
+  obj.data.meiye_balance = "999999";
+  obj.data.total_amount = "999999";
+  obj.data.meidou_balance = "999999";
+  obj.data.meiye_freeze_amount = "0";
+}
 
-objc = {
-    "meiye_balance": "25",
-    "total_amount": "25",
-    "meiye_freeze_amount": "0",
-    "my_credits": "My Credits",
-    "meidou_balance": "0"
-  },
-  "request_id": "68bd3c5147be42099e14867a08519e0a",
-  "success": true
-};
-$done({
-    body: JSON.stringify(obj)
+$done({ body: JSON.stringify(obj) });
